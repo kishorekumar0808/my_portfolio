dedd92ad1c85be7d3e191671f0564be21abc3f1c
kishore kumar <kikumar@dminc.com>
2025-04-22 17:02:01 +0530

    first commit

---
+++ src/data/user.js
@@ -0,0 +1,62 @@
+const INFO = {
+	main: {
+		title: "React portfolio",
+		name: "Kishore Kumar B.",
+		email: "leekishore0808@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		github: "https://github.com/kishorekumar0808",
+		linkedin: "https://www.linkedin.com/in/kishorekumar0808/",
+	},
+
+	homepage: {
+		title: "Full-stack web and mobile app developer.",
+		description:
+			"I'm a experienced developer in building scalable web and mobile applications using React and React Native. Worked on micro-frontend architecture, custom component development, responsive UI design, API integration, state management, and unit testing. Contributed to projects across healthcare, e-commerce, and research platforms, delivering cross-platform solutions with performance and usability in focus.",
+	},
+
+	about: {
+		title: "I’m Kishore Kumar. I live in Andhra Pradesh, where I design the future.",
+		description:
+			"I'm a passionate developer with a strong foundation in React and React Native. I thrive on challenges and enjoy creating innovative solutions that make a difference. My goal is to leverage technology to improve lives and drive positive change in the world.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "NxtTrends App",
+			description:
+				"Developed the NxtTrends e-commerce WebApp, enabling users to explore a variety of products, from clothing to toys and accessories. Utilized asynchronous HTTP requests with the Fetch API to seamlessly retrieve the NxtTrends product catalog from the server. Implemented dynamic routing and leveraged React's powerful context management for an efficient and responsive user experience. Enhanced the user interface with HTML and custom CSS3 styles to create an engaging and visually appealing platform. TechStack: React JS, JavaScript, HTML, CSS, React Router.",
+			logo: "https://static-00.iconduck.com/assets.00/react-icon-512x512-u6e60ayf.png",
+			linkText: "View Project",
+			link: "https://github.com/kishorekumar0808/NxtTrendsApp",
+		},
+
+		{
+			title: "NextShow App",
+			description:
+				"Designed and developed a user-friendly Mobile app, enabling users to book the movie tickets and effortlessly discover the latest movie releases, upcoming blockbusters, and popular films. Maintained a well-structured project folder architecture, meticulously organized components and screens, streamlining navigation and reducing development time by 15% faster feature implementation. Utilized React Native, TypeScript, and JavaScript to expedite cross-platform development, resulting in a remarkable 40% reduction in development time and costs. Successfully integrated data from the themoviedb API, providing users with up to date movie information, resulting in a 50% increase in user satisfaction and engagement. TechStack: React Native, TypeScript, JavaScript, JSX",
+			logo: "https://static-00.iconduck.com/assets.00/sdk-react-native-icon-1024x980-7ch6m2rf.png",
+			linkText: "View Project",
+			link: "https://github.com/kishorekumar0808/NxtWatch",
+		},
+
+		{
+			title: "Covid-19 Tracker",
+			description:
+				"Developed a Covid-19 Tracker WebApp, enabling users to access real-time data on Covid-19 cases, recoveries, and deaths. Utilized asynchronous HTTP requests with the Fetch API to seamlessly retrieve Covid-19 data from the server. Implemented dynamic routing and leveraged React's powerful context management for an efficient and responsive user experience. Enhanced the user interface with HTML and custom CSS3 styles to create an engaging and visually appealing platform. TechStack: React JS, JavaScript, HTML, CSS, React Router.",
+			logo: "https://static-00.iconduck.com/assets.00/react-icon-512x512-u6e60ayf.png",
+			linkText: "View Project",
+			link: "https://github.com/kishorekumar0808/CovidProject",
+		},
+	],
+};
+
+export default INFO;
